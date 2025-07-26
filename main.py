648a05cc4986dcab6b0d9de4ed8eb66a6598023e
M0Tarksu <mohamed84tarek@gmail.com>
2025-07-26 15:56:58 +0300

    added CricleShape

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     clock = pygame.time.Clock()
     dt = 0
 
@@ -16,7 +15,6 @@ def main():
 
         screen.fill((0, 0 ,0))
         pygame.display.flip()
-        
         dt = clock.tick(60)/1000
         
 if __name__ == "__main__":
