376de99401c8bdadcd035d3b7a8fc1823c07870b
M0Tarksu <mohamed84tarek@gmail.com>
2025-07-29 15:43:40 +0300

    added the cooldown to shots

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 from asteroid import Asteroid
+from shot import Shot
 
 def main():
     pygame.init()
@@ -15,13 +16,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     # setting Containers of action to defined groups
     Player.containers = (updatable, drawable) 
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable,drawable)
+    AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
-
     player = Player(SCREEN_WIDTH /2 , SCREEN_HEIGHT /2)
     dt = 0
 
